383a71235ad9814b836ba66ad662fab0ba36767d
ChandraSekhar Pola <chandraspola@outlook.com>
2026-01-06 13:54:31 +0000

    Initial project setup after testing

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,23 @@
+public class Calculator {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    // This method should return the result of subtracting y from x.
+    public int subtract(int x, int y) {
+        return 0;
+    }
+
+    // This method should return the result of multiplying x and y.
+    public int multiply(int x, int y) {
+        return 0;
+    }
+
+    // This method should return the result of diving x by y.
+    public int divide(int x, int y) {
+        return 0;
+    }
+
+
+}
